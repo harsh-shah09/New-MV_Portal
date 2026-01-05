f0b0aebde44f5e634d1e69d2296a37e12064738b
vivekmiskin <vivekmiskin07@gmail.com>
2026-01-05 16:22:23 +0530

    fix: Update S3 bucket URL in next.config.ts for dynamic configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://qftestv.s3.us-east-1.amazonaws.com/uploads/**')],
+    remotePatterns: [new URL(`https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/uploads/**`)],
   },
 };
 
